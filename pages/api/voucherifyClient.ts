676c96ee356942301e301014a1379c3ac83b4ed5
Patryk <patryk.smolarz1@gmail.com>
2022-08-18 12:18:41 +0200

    Revert "bug with request to client voucherify"
    This reverts commit 791f5bd323f64c33a91a6080634c18864646dc09.

--- pages/api/voucherifyClient.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config"
-import { VoucherifyServerSide } from "@voucherify/sdk";
-
-export const client = VoucherifyServerSide({
-  applicationId: process.env.VOUCHERIFY_APP_ID,
-  secretKey: process.env.VOUCHERIFY_SECRET_KEY,
-  apiUrl: 'https://<region>.api.voucherify.io', // optional
-});
