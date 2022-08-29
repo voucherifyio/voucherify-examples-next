fff40477b7005de8d56031dec26020bd1e18bd35
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-08-29 09:32:58 +0200

    redeem voucher

---
+++ pages/api/voucher-code-redemption/redeemVoucher.ts
@@ -0,0 +1,38 @@
+import { SimpleCustomer } from "@voucherify/sdk";
+import { NextApiRequest, NextApiResponse } from "next";
+import { calculateCartTotalAmount } from "../../../utils/calculateCartTotalAmount";
+import { mapCartItemToVoucherifyItem } from "../../../utils/mapCartItemToVoucherifyItem";
+import { validateRequestedCart } from "../../../utils/validateRequestedCart";
+import { client } from "../voucherifyClient";
+
+//@ts-ignore: customer dont need "id" for validate/redeem voucher
+const customer: SimpleCustomer = {
+  object: "customer",
+  source_id: "test_customer_id_1",
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  return new Promise(async () => {
+    const { voucherCode, filteredProducts } = req.body;
+    const items = validateRequestedCart(filteredProducts);
+    switch (req.method) {
+      case "POST":
+        const { result } = await client.redemptions.redeem(voucherCode, {
+          order: {
+            items: items.map(mapCartItemToVoucherifyItem),
+            amount: calculateCartTotalAmount(items),
+          },
+          customer: customer,
+        });
+        if (!result) {
+          return res.status(404).json({ error: "Voucher cannot be redeem" });
+        }
+        return res.status(200).json({ message: "Voucher redemeed" });
+      default:
+        return res.status(400).json({ error: "No response for this request" });
+    }
+  });
+}
