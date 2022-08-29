a0f489f4e8c704b20309b54ecddc6dd4e7f3aeae
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-08-29 09:32:40 +0200

    render products from session storage

---
+++ components/CheckoutProductsList/CheckoutProductsList.tsx
@@ -0,0 +1,24 @@
+import { Products } from "../../pages/voucher-code-redemption/types";
+import styles from "./CheckoutProductsList.module.css";
+import Image from "next/image";
+
+const CheckoutProductsList = ({ products }: Products) => {
+  return (
+    <div className={styles.summedProducts}>
+      {products.map((product) => {
+        return (
+          <div key={product.id} className={styles.eachProduct}>
+            <Image src={product.src} alt="" width={80} height={80} />
+            <div className={styles.eachProductName}>
+              <h6>{product.productName}</h6>
+              <p>Quantity {product.quantity}</p>
+            </div>
+            <span>{product.price}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CheckoutProductsList;
