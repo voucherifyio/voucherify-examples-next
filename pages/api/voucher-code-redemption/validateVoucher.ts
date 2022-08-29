f759a60a465748ac09e7de7d37c67e9f499118e4
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-08-29 09:29:01 +0200

    validate voucher

--- pages/api/voucher-code-redemption/validateVoucher.ts
+++ pages/api/voucher-code-redemption/validateVoucher.ts
@@ -1,49 +1,12 @@
 import { client } from "../voucherifyClient";
 import { NextApiRequest, NextApiResponse } from "next";
-import { defaultProducts } from "./defaultProducts";
-import { EachProduct } from "../../voucher-code-redemption/types";
+import { validateRequestedCart } from "../../../utils/validateRequestedCart";
+import { mapCartItemToVoucherifyItem } from "../../../utils/mapCartItemToVoucherifyItem";
+import { calculateCartTotalAmount } from "../../../utils/calculateCartTotalAmount";
 
 const customer = {
   source_id: "test_customer_id_1",
 };
-
-type RequestedItem = {
-  sku_id: string,
-  productName: string,
-  price: number,
-  id: number,
-  quantity: number,
-  length: number
-}
-
-const validateRequestedCart = (requestedCart: RequestedItem[]) => {
-  if (!requestedCart || !requestedCart.length) {
-    throw new Error("Requested cart should be an array of cart items");
-  }
-
-  return requestedCart
-    .map((requestedItem) => {
-      const item = defaultProducts.find(
-        (item) => requestedItem?.id && item.id === requestedItem.id
-      );
-      if (!item) {
-        return false;
-      }
-      return { ...item, quantity: requestedItem.quantity || 0 };
-    })
-    .filter((item) => !!item && item.quantity);
-};
-
-const mapCartItemToVoucherifyItem = (item) => ({
-  sku_id: item.productName,
-  price: item.price,
-  quantity: item.quantity,
-});
-
-const calculateCartTotalAmount = (items) =>
-  items
-    .reduce((sum, item) => sum + item.price * item.quantity * 100, 0)
-    .toFixed(2);
 
 export default async function handler(
   req: NextApiRequest,
@@ -62,14 +25,13 @@ export default async function handler(
             },
             customer: customer,
           });
-        console.log(valid);
         if (!valid) {
           return res.status(404).json({ error: "Voucher not found" });
         }
         return res.status(200).json({
-          order,
+          discount:
+            code === "FREE-SHIPPING" ? 0 : order?.total_applied_discount_amount,
           code,
-          discount,
         });
       default:
         return res.status(400).json({ error: "No response for this request" });
