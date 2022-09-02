f4bd16dd0d8ea32448cc13bba7d29658a36341ad
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-09-02 08:54:04 +0200

    checkout page main component

---
+++ pages/stacking-promotions/Checkout.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { MetaProperties } from "../../components/MetaProperties/Meta";
+import styles from "../../styles/CartAndCheckout.module.css";
+import { getCartAndVoucherFromSessionStorage } from "../../utils/stacking-promotions/sessionStorage";
+import { EachProduct } from "../types";
+import { VouchersProperties } from "../types";
+import FormPreview from "../../components/stacking-promotions/FormPreview/FormPreview";
+import CheckoutSummary from "../../components/stacking-promotions/CheckoutSummary/CheckoutSummary";
+
+const Checkout = () => {
+  const [products, setProducts] = useState<EachProduct[]>([]);
+  const [vouchersProperties, setVouchersProperties] =
+    useState<VouchersProperties>();
+
+  useEffect(() => {
+    const { storageProducts: products, vouchersProperties } =
+      getCartAndVoucherFromSessionStorage();
+    setProducts(products);
+    setVouchersProperties(vouchersProperties);
+  }, []);
+
+  return (
+    <>
+      <MetaProperties
+        title={"Stacking promotions"}
+        description={"Checkout view"}
+        keywords={"example, voucherify, cart shop"}
+      />
+      <div className={styles.checkoutPageWrapper}>
+        <FormPreview
+          products={products}
+          vouchersProperties={vouchersProperties as VouchersProperties}
+        />
+        {/* <CheckoutSummary
+          products={products}
+          vouchersProperties={vouchersProperties as VouchersProperties}
+        /> */}
+      </div>
+    </>
+  );
+};
+
+export default Checkout;
