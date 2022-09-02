35469337648bbe7b0ec5e6acd9bbbe0d6a789340
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-09-02 08:52:23 +0200

    validate stackable endpoint

---
+++ pages/api/stacking-promotions/validateStackable.ts
@@ -0,0 +1,62 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { calculateCartTotalAmount } from "../../../utils/calculateCartTotalAmount";
+import { removeDuplicatedPromoObjects } from "../../../utils/removeDuplicatePromo";
+import { validateRequestedCart } from "../../../utils/validateRequestedCart";
+import { PromotionStackableObj } from "../../types";
+import { client } from "../voucherifyClient";
+
+const customer = {
+  object: "customer",
+  source_id: "test_customer_id_1",
+};
+
+const promotionStackableObj: PromotionStackableObj = {
+  order: {
+    amount: 0,
+  },
+  customer: customer,
+  redeemables: [],
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { redeemables, filteredProducts } = req.body;
+  const items = validateRequestedCart(filteredProducts);
+  console.log(items)
+  promotionStackableObj.order.amount = calculateCartTotalAmount(items);
+  promotionStackableObj.redeemables = removeDuplicatedPromoObjects(redeemables)
+  console.log(promotionStackableObj);
+
+  switch (req.method) {
+    case "POST":
+      const { redeemables, order } = await client.validations.validateStackable(
+        promotionStackableObj
+      );
+      if (!redeemables) {
+        return res.status(404).json({
+          status: "error",
+          message: "Could not find voucher",
+        });
+      }
+      const [voucher] = redeemables?.filter(
+        (voucher) => voucher.status === "INAPPLICABLE"
+      );
+      console.log(voucher)
+      if (voucher?.result?.error) {
+        return res.status(404).json({
+          status: "error",
+          message: voucher.result.error.details,
+        });
+      }
+      return res.status(200).json({
+        amount: order?.amount,
+        itemsDiscountAmount: order?.items_discount_amount,
+        allDiscount: order?.total_discount_amount,
+        redeemables,
+      });
+    default:
+      return res.status(400).json({ error: "No response for this request" });
+  }
+}
