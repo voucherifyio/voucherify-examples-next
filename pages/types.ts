0672e59e805db418de7be12dc7ca8e442835660d
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-09-02 08:43:44 +0200

    main types to main pages folder

---
+++ pages/types.ts
@@ -0,0 +1,42 @@
+import { StackableRedeemableObject, StackableRedeemableParams } from "@voucherify/sdk";
+
+export type EachProduct = {
+  productName: string;
+  productDescription: string;
+  quantity: number;
+  price: number;
+  src: string;
+  id: number;
+};
+
+export type Products = {
+  products: EachProduct[];
+};
+
+export type Voucher = {
+  object: StackableRedeemableObject,
+  id: string,
+  order?: {
+    total_applied_discount_amount: number,
+    total_amount: number
+    items_discount_amount: number
+  } | undefined
+}
+
+export type PromotionStackableObj = {
+  order: {
+      amount: number;
+  };
+  customer: {
+      object: string;
+      source_id: string;
+  };
+  redeemables: StackableRedeemableParams[];
+}
+
+export type VouchersProperties = {
+  amount: number,
+  itemsDiscountAmount: number,
+  allDiscount: number,
+  redeemables: [Voucher]
+}
