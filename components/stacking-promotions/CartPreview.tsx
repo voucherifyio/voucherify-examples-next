7153b4fc150aef11dcc175d71991c418a238ff9a
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-09-08 15:11:08 +0200

    name RenderCartPreview => CartPreview

---
+++ components/stacking-promotions/CartPreview.tsx
@@ -0,0 +1,90 @@
+import styles from "../../styles/CartPreview/CartPreview.module.css";
+import Image from "next/image";
+import { Product, Voucher } from "../../pages/types";
+
+type Props = {
+  currentProducts: Product[];
+  setCurrentProducts: (products: Product[]) => void;
+  redeemables: Voucher[];
+  isActive: boolean;
+  onProductsQuantityChange: (
+    currentProducts: Product[],
+    voucherCodeValue: string,
+    redeemables: Voucher[]
+  ) => Promise<void>;
+};
+
+const RenderCartPreview = ({
+  currentProducts,
+  setCurrentProducts,
+  redeemables,
+  isActive,
+  onProductsQuantityChange,
+}: Props) => {
+  const products = [...currentProducts];
+
+  const incrementQuantity = async (index: number) => {
+    products[index].quantity++;
+    setCurrentProducts(products);
+    await onProductsQuantityChange(currentProducts, "", redeemables);
+  };
+
+  const decrementQuantity = async (index: number) => {
+    if (currentProducts[index].quantity <= 0) return;
+    products[index].quantity--;
+    setCurrentProducts(products);
+    await onProductsQuantityChange(currentProducts, "", redeemables);
+  };
+
+  return (
+    <div className={styles.cartSummary}>
+      <h2>Item summary</h2>
+      <div className={styles.cartSummaryList}>
+        {currentProducts.map((product, index) => {
+          return (
+            <div
+              className={styles.productItem}
+              key={`${product.id}-${product.quantity}`}
+            >
+              <Image
+                src={product.src}
+                alt="Product image"
+                width={70}
+                height={70}
+              />
+              <div className={styles.nameAndDescription}>
+                <span>{product.productName}</span>
+                <span>{product.productDescription}</span>
+              </div>
+              <div className={styles.countingProducts}>
+                <button
+                  className={styles.decrement}
+                  disabled={isActive}
+                  onClick={async () => await decrementQuantity(index)}
+                >
+                  -
+                </button>
+                <input
+                  className={styles.quantityInput}
+                  type="number"
+                  defaultValue={product.quantity}
+                  disabled={true}
+                />
+                <button
+                  className={styles.increment}
+                  disabled={isActive}
+                  onClick={async () => await incrementQuantity(index)}
+                >
+                  +
+                </button>
+              </div>
+              <span className={styles.price}>{product.price}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default RenderCartPreview;
