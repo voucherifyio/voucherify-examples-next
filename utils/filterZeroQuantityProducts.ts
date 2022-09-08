4d11288a4cebc90b89050f01134b79c5c0105532
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-09-08 12:00:04 +0200

    optional chaining only

--- utils/filterZeroQuantityProducts.ts
+++ utils/filterZeroQuantityProducts.ts
@@ -1,7 +1,7 @@
-import { EachProduct } from "../pages/types";
+import { Product } from "../pages/types";
 
-export const filterZeroQuantityProducts = (currentProducts: EachProduct[]) => {
-  const filteredProducts = currentProducts && currentProducts
+export const filterZeroQuantityProducts = (currentProducts: Product[]) => {
+  const filteredProducts = currentProducts
     ?.filter((product) => product.quantity !== 0)
     ?.map((product) => {
       return { id: product.id, quantity: product.quantity };
