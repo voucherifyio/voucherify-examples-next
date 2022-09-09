67ed60980b24b4f835ef98d816a8d3c6724651b5
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-09-09 16:41:54 +0200

    env variable

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: "https://voucherify-examples-next.herokuapp.com/"
+  }
 }
 
 module.exports = nextConfig
