ddf95ed9d6c390d86b1183000a0e3331d8725445
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-09-13 09:23:20 +0200

    RenderCardPreview => CartPreview

--- components/tiered-cart-promotions/CartPreview.tsx
+++ components/tiered-cart-promotions/CartPreview.tsx
@@ -1,34 +1,54 @@
-import styles from "../../../styles/RenderCartPreview/RenderCartPreview.module.css";
+import styles from "../../styles/CartPreview/CartPreview.module.css";
 import Image from "next/image";
-import { Product, Voucher } from "../../types";
+import { Product, Voucher } from "../types";
+import RewardsPanel from "./RewardsPanel/RewardsPanel";
+import { useEffect } from "react";
 
 type Props = {
   currentProducts: Product[];
   setCurrentProducts: (products: Product[]) => void;
   redeemables: Voucher[];
+  onProductsQuantityChange: (currentProducts: Product[]) => Promise<void>;
 };
 
 const RenderCartPreview = ({
   currentProducts,
   setCurrentProducts,
   redeemables,
+  onProductsQuantityChange,
 }: Props) => {
-  const products = [...currentProducts];
+
+  useEffect(() => {
+    onProductsQuantityChange(currentProducts);
+  }, [currentProducts, onProductsQuantityChange]);
 
   const incrementQuantity = (index: number) => {
-    products[index].quantity++;
-    setCurrentProducts(products);
+    setCurrentProducts([
+      ...currentProducts.slice(0, index),
+      {
+        ...currentProducts[index],
+        quantity: currentProducts[index].quantity + 1,
+      },
+      ...currentProducts.slice(index + 1),
+    ]);
   };
 
   const decrementQuantity = (index: number) => {
     if (currentProducts[index].quantity <= 0) return;
-    products[index].quantity--;
-    setCurrentProducts(products);
+    setCurrentProducts([
+      ...currentProducts.slice(0, index),
+      {
+        ...currentProducts[index],
+        quantity: currentProducts[index].quantity - 1,
+      },
+      ...currentProducts.slice(index + 1),
+    ]);
   };
 
   return (
     <div className={styles.cartSummary}>
       <h2>Item summary</h2>
+      <RewardsPanel />
       <div className={styles.cartSummaryList}>
         {currentProducts.map((product, index) => {
           return (
