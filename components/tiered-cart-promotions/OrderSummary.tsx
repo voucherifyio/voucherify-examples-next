de3002c22e3aaff668d60e6a02c0b6b65b2eeb9d
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-09-14 15:00:39 +0200

    coupon count change

--- components/tiered-cart-promotions/OrderSummary.tsx
+++ components/tiered-cart-promotions/OrderSummary.tsx
@@ -1,39 +1,33 @@
 import styles from "../../styles/OrderSummary/OrderSummary.module.css";
-import { VouchersProperties, Product, Voucher } from "../types";
+import { Product, PromotionTier } from "../types";
 import Image from "next/image";
 import { saveCartAndVoucherInSessionStorage } from "../../utils/tiered-cart-promotions/sessionStorage";
 import { useRouter } from "next/router";
 import { sumTotalPrice } from "../../utils/sumTotalPrice";
 
 type Props = {
-  vouchersProperties: VouchersProperties;
+  vouchersProperties: PromotionTier[];
   currentProducts: Product[];
   error: string;
 };
-
 const OrderSummary = ({
   vouchersProperties,
   currentProducts,
-  error
+  error,
 }: Props) => {
   const router = useRouter();
-console.log(vouchersProperties)
+  const promotions =
+    vouchersProperties?.reduce(
+      (sum, tier) => sum + tier.total_applied_discount_amount,
+      0
+    ) / 100;
   return (
     <div className={styles.orderSummary}>
       <h2>Order summary</h2>
       <div className={styles.totalOrderWrapper}>
         <h4>
           Subtotal:
-          <span>
-            ${sumTotalPrice(currentProducts)}
-            {/* {vouchersProperties?.itemsDiscountAmount
-              ? (
-                  (vouchersProperties?.amount -
-                    vouchersProperties?.itemsDiscountAmount) /
-                  100
-                ).toFixed(2)
-              : sumTotalPrice(currentProducts)} */}
-          </span>
+          <span>${sumTotalPrice(currentProducts)}</span>
         </h4>
         <h4>
           Shipping:<span>Calculated at next step</span>
@@ -61,20 +55,12 @@ console.log(vouchersProperties)
         <div className={styles.voucherFormErrorWrapper}></div>
         <div className={styles.promotionsWrapper}>
           <h4>Promotions:</h4>
-          {vouchersProperties?.redeemables?.map((voucher: Voucher) => {
+          {vouchersProperties?.map((tier: PromotionTier) => {
             return (
-              <div key={voucher.id} className={styles.promotionHolder}>
-                <h5>
-                  {voucher.object === "promotion_tier"
-                    ? "Promotion tier"
-                    : voucher.id}
-                </h5>
+              <div key={tier.id} className={styles.promotionHolder}>
+                <h5>{tier.name}</h5>
                 <div>
-                  {voucher.id === "FREE-SHIPPING"
-                    ? "Free shipping"
-                    : `$${(
-                        voucher?.order!.total_applied_discount_amount / 100
-                      ).toFixed(2)}`}
+                  {`$${(tier?.total_applied_discount_amount / 100).toFixed(2)}`}
                 </div>
               </div>
             );
@@ -88,25 +74,23 @@ console.log(vouchersProperties)
         <h4 className={styles.discountPriceHolder}>
           All Your Discounts:
           <span className={styles.allDiscounts}>
-            {/* {vouchersProperties?.allDiscount
-              ? `$${(
-                  (vouchersProperties?.allDiscount -
-                    vouchersProperties?.itemsDiscountAmount) /
-                    100 || vouchersProperties?.allDiscount / 100
-                ).toFixed(2)}`
-              : "n/a"} */}
+            {vouchersProperties.length
+              ? `$${
+                  vouchersProperties?.reduce(
+                    (sum, tier) => sum + tier.total_applied_discount_amount,
+                    0
+                  ) / 100
+                }`
+              : "n/a"}
           </span>
         </h4>
         <h4>
           Grand total:
           <span id={styles.grandTotal}>
             $
-            {/* {vouchersProperties?.redeemables
-              ? (
-                  vouchersProperties?.redeemables?.at(-1)?.order!
-                    ?.total_amount / 100
-                ).toFixed(2)
-              : sumTotalPrice(currentProducts)} */}
+            {(parseFloat(sumTotalPrice(currentProducts)) - promotions).toFixed(
+              2
+            )}
           </span>
         </h4>
       </div>
@@ -122,7 +106,7 @@ console.log(vouchersProperties)
             vouchersProperties,
             currentProducts
           );
-          router.push("/stacking-promotions/Checkout");
+          router.push("/tiered-cart-promotions/Checkout");
         }}
       >
         Checkout
