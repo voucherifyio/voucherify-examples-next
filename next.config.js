df8896f32f3b02bf79356e45ce4ec5a277085ce3
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-09-15 16:02:23 +0200

    removed env for heroku

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    NEXT_PUBLIC_BACKEND_URL: "https://voucherify-examples-next.herokuapp.com/"
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
